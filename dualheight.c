ff51ab83736f8a78c1944d26c8dea6958796164e
Debasish Banerjee <cygnus2@gmail.com>
2012-10-02 00:12:56 +0200

    Checked neighbours with 4x4x4 lattice repeat with 6x6x6 lattice

--- dualheight.c
+++ dualheight.c
@@ -4,6 +4,8 @@
 
 #include<stdio.h>
 #include<math.h>
+#include<string.h>
+#include<stdlib.h>
 #include "mt19937ar.h"
 
 /* Parameters                      *
@@ -16,36 +18,55 @@
  * imeas      : # of measurements  *
  * iskp       : # of iter/meas     */
 
-#define LX 30
-#define LY 30
-#define LT 30
-
+int LX,LY,LT,VOL,VOL2;
 double p1,p2;
 double j,lam,beta,eps;
-int mark[ncube],markl[nlink];
-double hist[31];
-int VOL=LX*LY*LT;
 int SEED;
 
 /* pointer variables for neighbours */
-int fx[VOL],bx[VOL],fy[VOL],by[VOL],ft[VOL],bt[VOL];
-int ix[VOL],iy[VOL],it[VOL];
-
+int *neigh[6];
+/* checkerboard labeling for sites */
+int *ixc,*iyc,*itc;
 /* Field variables */
-int ising[VOL];
+int *ising;
 
 int main(){
-    
-   extern void neighbours();
-   extern double ran();
-   
 
    int ieq,imeas,iskp;
+   int i,readval;    
+   extern void neighbours();
+   extern void neighchk();
+   extern double ran();
+
+   FILE *fptr;
+   char st[20];
+   /* read file */
+   fptr=fopen("QUEUE","r");
+   if(fptr == NULL) {printf("QUEUE error.\n"); exit(1);}
+   readval = fscanf(fptr,"%s %d\n",st,&LX);
+   if(readval == -1) printf("Error\n");
+   readval = fscanf(fptr,"%s %d\n",st,&LY);
+   if(readval == -1) printf("Error\n");
+   readval = fscanf(fptr,"%s %d\n",st,&LT);
+   if(readval == -1) printf("Error\n");
+   fclose(fptr);
+
+   VOL = LX*LY*LT;
+   VOL2= VOL/2;
+
+   /* allocate memory */
+   ixc   = (int *)malloc(VOL*sizeof(int));
+   iyc   = (int *)malloc(VOL*sizeof(int));
+   itc   = (int *)malloc(VOL*sizeof(int));
+   ising = (int *)malloc(VOL*sizeof(int));
+   for(i=0;i<=5;i++)
+     neigh[i] = (int *)malloc(VOL*sizeof(int));
+
    /* Set parameters */
    j=1.0;
    lam = 0.0;
    beta=3.6;
-   eps=2.0/((double)nt);
+   eps=2.0/((double)LT);
    ieq=100;
    imeas=100;
    iskp=1;
@@ -58,19 +79,22 @@ int main(){
   neighbours();
 
   printf("Cluster Algorithm for the U(1) quantum link model\n");
-  printf("Nx=%d, Ny=%d, Nt=%d\n",nx,ny,nt);
+  printf("Nx=%d, Ny=%d, Nt=%d\n",LX,LY,LT);
   printf("beta=%2.3f; J=%2.3f; lam=%2.3f\n",beta,j,lam);
   printf("Starting seed=%d\n",SEED);
 
+  neighchk();
   /* Define the probabilities */
   double x,p1,p2,coshx,sinhx;
-  x     = eps*beta*j
+  x     = eps*beta*j;
   coshx = (exp(x)+exp(-x))/2.0;
   sinhx = (exp(x)-exp(-x))/2.0;
   p1    = exp(-eps*lam)*coshx;
   p2    = exp(-eps*lam)*sinhx;
 
-
+  /* free memory */
+  free(ixc); free(iyc); free(itc);
+  for(i=0;i<=5;i++) free(neigh[i]);
   return 0;
  }
 
@@ -79,263 +103,75 @@ double ran(){
   return genrand_real2();
 }
 
+int convert(int x,int y,int t){
+  int n,parity;
+  parity = (x+y+t)%2;
+  n = t*LY*LX + y*LX + x;
+  n = n/2;
+  if(parity == 1) n = n + VOL2;
+  return n;
+}
 
 void neighbours(){
  extern int convert(int,int,int);
- int i;
+ int i,ieven,iodd,parity;
  int ixp1,ixm1,iyp1,iym1,itp1,itm1;
- int ilxyt1,ilxyt2;
- /* relate cartesian and lattice co-ordinates */
- i=0;
- for(it=0;it<nt;it++){
- for(iy=0;iy<ny;iy++){
- for(ix=0;ix<nx;ix++){
-  ixc[i]=ix;
-  iyc[i]=iy;
-  itc[i]=it;
-  i++;
- } } } 
- /* define the neighbours */
- for(ix=0;ix<nx;ix++){
-  ixp1=(ix+1)%nx;
-  ixm1=(ix-1+nx)%nx;
-  for(iy=0;iy<ny;iy++){
-   iyp1=(iy+1)%ny;
-   iym1=(iy-1+ny)%ny;
-   for(it=0;it<nt;it++){
-    itp1=(it+1)%nt;
-    itm1=(it-1+nt)%nt;
+ int ix,iy,it;
+ /* serial to checkerboard list */ 
+ ieven=iodd=0;
+ for(it=0;it<LT;it++){
+ itp1=(it+1)%LT;
+ itm1=(it-1+LT)%LT;
+ for(iy=0;iy<LY;iy++){
+ iyp1=(iy+1)%LY;
+ iym1=(iy-1+LY)%LY;
+ for(ix=0;ix<LX;ix++){
+ ixp1=(ix+1)%LX;
+ ixm1=(ix-1+LX)%LX;
 
-/* first if starts */
-     if(it%2==0){ /* select the even time slices */
-/* second if starts */
-     if((ix+iy)%2==0){
-      /* fwd neighbours */
-      neigh[ilxyt1][0][0] = il[convert(ixp1,iy,it)][1];
-      neigh[ilxyt1][1][0] = il[convert(ix,iyp1,it)][0];
-      neigh[ilxyt1][2][0] = il[convert(ix,iy,it)][1];
-      neigh[ilxyt1][3][0] = il[convert(ix,iy,itp1)][0];
-      neigh[ilxyt1][4][0] = il[convert(ixp1,iy,itp1)][1];
-      neigh[ilxyt1][5][0] = il[convert(ix,iyp1,itp1)][0];
-      neigh[ilxyt1][6][0] = il[convert(ix,iy,itp1)][1];
-      neigh[ilxyt2][0][0] = il[convert(ix,iyp1,it)][0];
-      neigh[ilxyt2][1][0] = il[convert(ixp1,iy,it)][1];
-      neigh[ilxyt2][2][0] = il[convert(ix,iy,it)][0];
-      neigh[ilxyt2][3][0] = il[convert(ix,iy,itp1)][1];
-      neigh[ilxyt2][4][0] = il[convert(ix,iyp1,itp1)][0];
-      neigh[ilxyt2][5][0] = il[convert(ixp1,iy,itp1)][1];
-      neigh[ilxyt2][6][0] = il[convert(ix,iy,itp1)][0];
-      /* bwd neighbours */
-      neigh[ilxyt1][0][1] = il[convert(ix,iym1,it)][1];
-      neigh[ilxyt1][1][1] = il[convert(ix,iym1,it)][0];
-      neigh[ilxyt1][2][1] = il[convert(ixp1,iym1,it)][1];
-      neigh[ilxyt1][3][1] = il[convert(ix,iy,itm1)][0];
-      neigh[ilxyt1][4][1] = il[convert(ix,iym1,itm1)][1];
-      neigh[ilxyt1][5][1] = il[convert(ix,iym1,itm1)][0];
-      neigh[ilxyt1][6][1] = il[convert(ixp1,iym1,itm1)][1];
-      neigh[ilxyt2][0][1] = il[convert(ixm1,iy,it)][0];
-      neigh[ilxyt2][1][1] = il[convert(ixm1,iy,it)][1];
-      neigh[ilxyt2][2][1] = il[convert(ixm1,iyp1,it)][0];
-      neigh[ilxyt2][3][1] = il[convert(ix,iy,itm1)][1];
-      neigh[ilxyt2][4][1] = il[convert(ixm1,iy,itm1)][0];
-      neigh[ilxyt2][5][1] = il[convert(ixm1,iy,itm1)][1];
-      neigh[ilxyt2][6][1] = il[convert(ixm1,iyp1,itm1)][0];
-    }
-    else{
-      /* fwd neighbours */
-      neigh[ilxyt1][0][0] = il[convert(ix,iym1,it)][1];
-      neigh[ilxyt1][1][0] = il[convert(ix,iym1,it)][0];
-      neigh[ilxyt1][2][0] = il[convert(ixp1,iym1,it)][1];
-      neigh[ilxyt1][3][0] = il[convert(ix,iy,itp1)][0];
-      neigh[ilxyt1][4][0] = il[convert(ix,iym1,itp1)][1];
-      neigh[ilxyt1][5][0] = il[convert(ix,iym1,itp1)][0];
-      neigh[ilxyt1][6][0] = il[convert(ixp1,iym1,itp1)][1];
-      neigh[ilxyt2][0][0] = il[convert(ixm1,iy,it)][0];
-      neigh[ilxyt2][1][0] = il[convert(ixm1,iy,it)][1];
-      neigh[ilxyt2][2][0] = il[convert(ixm1,iyp1,it)][0];
-      neigh[ilxyt2][3][0] = il[convert(ix,iy,itp1)][1];
-      neigh[ilxyt2][4][0] = il[convert(ixm1,iy,itp1)][0];
-      neigh[ilxyt2][5][0] = il[convert(ixm1,iy,itp1)][1];
-      neigh[ilxyt2][6][0] = il[convert(ixm1,iyp1,itp1)][0];
-      /* bwd neighbours */
-      neigh[ilxyt1][0][1] = il[convert(ixp1,iy,it)][1];
-      neigh[ilxyt1][1][1] = il[convert(ix,iyp1,it)][0];
-      neigh[ilxyt1][2][1] = il[convert(ix,iy,it)][1];
-      neigh[ilxyt1][3][1] = il[convert(ix,iy,itm1)][0];
-      neigh[ilxyt1][4][1] = il[convert(ixp1,iy,itm1)][1];
-      neigh[ilxyt1][5][1] = il[convert(ix,iyp1,itm1)][0];
-      neigh[ilxyt1][6][1] = il[convert(ix,iy,itm1)][1];
-      neigh[ilxyt2][0][1] = il[convert(ix,iyp1,it)][0];
-      neigh[ilxyt2][1][1] = il[convert(ixp1,iy,it)][1];
-      neigh[ilxyt2][2][1] = il[convert(ix,iy,it)][0];
-      neigh[ilxyt2][3][1] = il[convert(ix,iy,itm1)][1];
-      neigh[ilxyt2][4][1] = il[convert(ix,iyp1,itm1)][0];
-      neigh[ilxyt2][5][1] = il[convert(ixp1,iy,itm1)][1];
-      neigh[ilxyt2][6][1] = il[convert(ix,iy,itm1)][0];
-    }
-/* second if over */
-   }
-/* else for first if */
-   else{
-/* third if starts */
-    if((ix+iy)%2==1){
-      /* fwd neighbours */
-      neigh[ilxyt1][0][0] = il[convert(ixp1,iy,it)][1];
-      neigh[ilxyt1][1][0] = il[convert(ix,iyp1,it)][0];
-      neigh[ilxyt1][2][0] = il[convert(ix,iy,it)][1];
-      neigh[ilxyt1][3][0] = il[convert(ix,iy,itp1)][0];
-      neigh[ilxyt1][4][0] = il[convert(ixp1,iy,itp1)][1];
-      neigh[ilxyt1][5][0] = il[convert(ix,iyp1,itp1)][0];
-      neigh[ilxyt1][6][0] = il[convert(ix,iy,itp1)][1];
-      neigh[ilxyt2][0][0] = il[convert(ix,iyp1,it)][0];
-      neigh[ilxyt2][1][0] = il[convert(ixp1,iy,it)][1];
-      neigh[ilxyt2][2][0] = il[convert(ix,iy,it)][0];
-      neigh[ilxyt2][3][0] = il[convert(ix,iy,itp1)][1];
-      neigh[ilxyt2][4][0] = il[convert(ix,iyp1,itp1)][0];
-      neigh[ilxyt2][5][0] = il[convert(ixp1,iy,itp1)][1];
-      neigh[ilxyt2][6][0] = il[convert(ix,iy,itp1)][0];
-      /* bwd neighbours */
-      neigh[ilxyt1][0][1] = il[convert(ix,iym1,it)][1];
-      neigh[ilxyt1][1][1] = il[convert(ix,iym1,it)][0];
-      neigh[ilxyt1][2][1] = il[convert(ixp1,iym1,it)][1];
-      neigh[ilxyt1][3][1] = il[convert(ix,iy,itm1)][0];
-      neigh[ilxyt1][4][1] = il[convert(ix,iym1,itm1)][1];
-      neigh[ilxyt1][5][1] = il[convert(ix,iym1,itm1)][0];
-      neigh[ilxyt1][6][1] = il[convert(ixp1,iym1,itm1)][1];
-      neigh[ilxyt2][0][1] = il[convert(ixm1,iy,it)][0];
-      neigh[ilxyt2][1][1] = il[convert(ixm1,iy,it)][1];
-      neigh[ilxyt2][2][1] = il[convert(ixm1,iyp1,it)][0];
-      neigh[ilxyt2][3][1] = il[convert(ix,iy,itm1)][1];
-      neigh[ilxyt2][4][1] = il[convert(ixm1,iy,itm1)][0];
-      neigh[ilxyt2][5][1] = il[convert(ixm1,iy,itm1)][1];
-      neigh[ilxyt2][6][1] = il[convert(ixm1,iyp1,itm1)][0];
-    }
-    else{
-      /* fwd neighbours */
-      neigh[ilxyt1][0][0] = il[convert(ix,iym1,it)][1];
-      neigh[ilxyt1][1][0] = il[convert(ix,iym1,it)][0];
-      neigh[ilxyt1][2][0] = il[convert(ixp1,iym1,it)][1];
-      neigh[ilxyt1][3][0] = il[convert(ix,iy,itp1)][0];
-      neigh[ilxyt1][4][0] = il[convert(ix,iym1,itp1)][1];
-      neigh[ilxyt1][5][0] = il[convert(ix,iym1,itp1)][0];
-      neigh[ilxyt1][6][0] = il[convert(ixp1,iym1,itp1)][1];
-      neigh[ilxyt2][0][0] = il[convert(ixm1,iy,it)][0];
-      neigh[ilxyt2][1][0] = il[convert(ixm1,iy,it)][1];
-      neigh[ilxyt2][2][0] = il[convert(ixm1,iyp1,it)][0];
-      neigh[ilxyt2][3][0] = il[convert(ix,iy,itp1)][1];
-      neigh[ilxyt2][4][0] = il[convert(ixm1,iy,itp1)][0];
-      neigh[ilxyt2][5][0] = il[convert(ixm1,iy,itp1)][1];
-      neigh[ilxyt2][6][0] = il[convert(ixm1,iyp1,itp1)][0];
-      /* bwd neighbours */
-      neigh[ilxyt1][0][1] = il[convert(ixp1,iy,it)][1];
-      neigh[ilxyt1][1][1] = il[convert(ix,iyp1,it)][0];
-      neigh[ilxyt1][2][1] = il[convert(ix,iy,it)][1];
-      neigh[ilxyt1][3][1] = il[convert(ix,iy,itm1)][0];
-      neigh[ilxyt1][4][1] = il[convert(ixp1,iy,itm1)][1];
-      neigh[ilxyt1][5][1] = il[convert(ix,iyp1,itm1)][0];
-      neigh[ilxyt1][6][1] = il[convert(ix,iy,itm1)][1];
-      neigh[ilxyt2][0][1] = il[convert(ix,iyp1,it)][0];
-      neigh[ilxyt2][1][1] = il[convert(ixp1,iy,it)][1];
-      neigh[ilxyt2][2][1] = il[convert(ix,iy,it)][0];
-      neigh[ilxyt2][3][1] = il[convert(ix,iy,itm1)][1];
-      neigh[ilxyt2][4][1] = il[convert(ix,iyp1,itm1)][0];
-      neigh[ilxyt2][5][1] = il[convert(ixp1,iy,itm1)][1];
-      neigh[ilxyt2][6][1] = il[convert(ix,iy,itm1)][0];
-    }
-/* third if over */
-   }
-/* closing for else corresponding to first if */
-/* next 3 braces closing for the three for loops */
-   }
-  }
+ parity = (ix+iy+it)%2;
+ /* create checkerboard neighbors */
+ /*     2
+  *  3  x  1
+  *     4
+  * neigh is a double indexed array. The second index tracks the
+  * dual site x, while the first index tracks the 6-spin interaction.
+  * 1,2,3,4 show the spins on time-slice t as seen above.
+  * 0 denotes the spin at x in time t-1
+  * 5 denotes the spin at x in time t+1
+  */
+ if(parity==0){
+  ixc[ieven]=ix;
+  iyc[ieven]=iy;
+  itc[ieven]=it;
+  neigh[0][ieven]=convert(ix,iy,itm1);
+  neigh[1][ieven]=convert(ixp1,iy,it);
+  neigh[2][ieven]=convert(ix,iyp1,it);
+  neigh[3][ieven]=convert(ixm1,iy,it);
+  neigh[4][ieven]=convert(ix,iym1,it);
+  neigh[5][ieven]=convert(ix,iy,itp1);
+  ieven++;
+ }
+ else{
+  ixc[VOL2+iodd]=ix;
+  iyc[VOL2+iodd]=iy;
+  itc[VOL2+iodd]=it;
+  neigh[0][VOL2+iodd]=convert(ix,iy,itm1);
+  neigh[1][VOL2+iodd]=convert(ixp1,iy,it);
+  neigh[2][VOL2+iodd]=convert(ix,iyp1,it);
+  neigh[3][VOL2+iodd]=convert(ixm1,iy,it);
+  neigh[4][VOL2+iodd]=convert(ix,iym1,it);
+  neigh[5][VOL2+iodd]=convert(ix,iy,itp1);
+  iodd++;
+ }
+ }}}
+ if((ieven!=VOL2)||(iodd!=VOL2)) printf("ERROR\n");
  }
 
-  /* label cubes */
-  i=0;
-  for(it=0;it<nt;it++){
-   itp1 = (it+1)%nt;
-   for(iy=0;iy<ny;iy++){
-    iyp1 = (iy+1)%ny;
-    for(ix=0;ix<nx;ix++){
-     ixp1 = (ix+1)%nx;
-     if((ix+iy+it)%2==0){
-      icube[il[convert(ix,iy,it)][0]][0] = i;
-      icube[il[convert(ix,iy,it)][1]][0] = i;
-      icube[il[convert(ixp1,iy,it)][1]][0]=i;
-      icube[il[convert(ix,iyp1,it)][0]][0]=i;
-      icube[il[convert(ix,iy,itp1)][0]][1]=i;
-      icube[il[convert(ix,iy,itp1)][1]][1]=i;
-      icube[il[convert(ixp1,iy,itp1)][1]][1]=i;
-      icube[il[convert(ix,iyp1,itp1)][0]][1]=i;
-      i++;
-     }
-    }
-   }
-  }
+void neighchk(){
+ int p;
+ for(p=0;p<VOL;p++){
+  printf("% d %d %d %d %d %d %d %d %d %d\n",p,ixc[p],iyc[p],itc[p],neigh[0][p],
+    neigh[1][p],neigh[2][p],neigh[3][p],neigh[4][p],neigh[5][p]);
  }
-
-/* This makes a random start for all links */
-void ranstrt(){
-  int ix,iy,id,it;
-  int ilink;
-  for(ix=0;ix<nx;ix++){
-  for(iy=0;iy<ny;iy++){
-  for(id=0;id<2;id++){
-    ilink=0;
-    if(genrand_real3()>0.5) ilink=1;
-    for(it=0;it<nt;it++){
-     ising[il[convert(ix,iy,it)][id]]=ilink;
-    }
-  } } }
-}
-
-/* This makes a twist start for starting configuration */
-void twiststrt(){
-  int ix,iy,it;
-  for(ix=0;ix<nx;ix++){
-  for(iy=0;iy<ny;iy++){
-  for(it=0;it<nt;it++){
-   if((ix+iy+it)%2==0){
-    ising[il[convert(ix,iy,it)][0]]=1;
-    ising[il[convert(ix,iy,it)][1]]=0;
-   }
-   else{
-    ising[il[convert(ix,iy,it)][0]]=0;
-    ising[il[convert(ix,iy,it)][1]]=1;
-   }
-  } } }
-}
-
-/* Check for forbidden configurations */
-void check(int *nontriv, int *nwind){
-  int il,ic;
-  int il1,il2,il3,il4,il5,il6,il7;
-  nontriv=0;
-  nwind=0;
-  for(il=0;il<nlink;il++){
-  for(ic=0;ic<2;ic++){
-   il1=neigh[il][1][ic];
-   il2=neigh[il][2][ic];
-   il3=neigh[il][3][ic];
-   il4=neigh[il][4][ic];
-   il5=neigh[il][5][ic];
-   il6=neigh[il][6][ic];
-   il7=neigh[il][7][ic];
-   if((ising[il]==ising[il4])&&(ising[il1]==ising[il5])&&(ising[il2]==ising[il6])&&(ising[il3]==ising[il7]))
-    continue;
-   else{ /* count the non-trivial cubes */
-    if((ising[il]==ising[il1])&&(ising[il]!=ising[il2])&&(ising[il]!=ising[il3])&&(ising[il]!=ising[il4])&&
-       (ising[il]!=ising[il5])&&(ising[il]==ising[il6])&&(ising[il]==ising[il7])){
-        nontriv++;
-	/* calculate the winding number */
-        if((ixc[il]==0)&&(iyc[il]==0)&&(itc[il]%2==0)&&(idir[il]==0)&&(ic==1))
-          nwind += 2*ising[il]-1;
-       }
-     else{
-       printf("Forbidden configuration\n");
-       exit(0);
-     }
-   }
-  }
-  }
-}
+ }
